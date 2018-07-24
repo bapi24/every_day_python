f697b93706faca168c1ce3addb968b5920d2acdf
Bapi Raj Loya <bapi.loya@gmail.com>
2018-07-24 13:54:59 -0700

    unit tests

--- yaml_parser.py
+++ yaml_parser.py
@@ -1,5 +1,6 @@
 import yaml
-
+'''
+# method 1
 filename = "yaml_file.yml"
 
 with open(filename) as f:
@@ -7,3 +8,35 @@ with open(filename) as f:
 
 
 print(content['lakers'])
+
+
+#method 2
+def read_content(filename):
+    with open(filename) as f:
+        return yaml.load(f)
+
+content = read_content("yaml_file.yml")
+
+print(content)
+'''
+# method 3
+class yaml_parser:
+    def __init__(self):
+        self._config_file = 'config.yml'
+        dict1 = {}
+    
+    def _read_config(self):
+        filename = self._config_file
+
+        with open(filename) as f:
+            content = yaml.load(f)
+        
+        return content
+    
+    def _get_config(self, content):
+        print(f"The contents of config are: {content}")
+
+yp = yaml_parser()
+content = yp._read_config()
+# print(content)
+yp._get_config(content)
