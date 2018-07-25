e8c5ec8b4f7bbd41360341c0ca434b94e2fd2c24
Bapi Raj Loya <bapi.loya@gmail.com>
2018-07-25 07:37:23 -0700

    git change

--- yaml_parser.py
+++ yaml_parser.py
@@ -1,9 +1,19 @@
 import yaml
-
+'''
 filename = "yaml_file.yml"
 
 with open(filename) as f:
     content = yaml.load(f)
 
-
 print(content['lakers'])
+'''
+# filename = "yaml_file.yml"
+
+def read_yaml(filename):
+    # self.filename = filename
+    with open(filename) as f:
+        content = yaml.load(f)
+
+    print(content)    
+
+read_yaml("yaml_file.yml")
